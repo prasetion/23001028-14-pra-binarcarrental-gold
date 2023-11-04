fd515a18ed1e3024c26290e60e24bd1ad4b4e9f5
prasetion <prasetion@gmail.com>
2023-11-04 20:50:17 +0700

    add faq section

---
+++ src/components/FAQ/index.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const FAQ = () => {
+  return (
+    <div className="faq-container">
+      <div className="faq-container-left">
+        <div className="faq-container-title">Frequently Asked Question</div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+      </div>
+      <div className="faq-container-right">Rigth</div>
+    </div>
+  );
+};
+
+export default FAQ;
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -2,12 +2,14 @@ import "./style.css";
 import Footer from "../../components/Footer";
 import WhyUs from "../../components/WhyUs";
 import Banner from "../../components/Banner";
+import FAQ from "../../components/FAQ";
 
 const LandingPage = () => {
   return (
     <div>
       <WhyUs />
       <Banner />
+      <FAQ />
       <Footer />
     </div>
   );
