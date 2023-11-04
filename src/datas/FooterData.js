d666beed9342de3ee91d7a46b05dde04e7b4d75c
prasetion <prasetion@gmail.com>
2023-11-04 09:09:14 +0700

    add data container export and slicing div for each section in footer

---
+++ src/datas/FooterData.js
@@ -0,0 +1,14 @@
+export const footerAddress = [
+  "Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000",
+  "binarcarrental@gmail.com",
+  "081-233-334-808",
+];
+
+export const footerNavigation = [
+  "Our services",
+  "Why Us",
+  "Testimonial",
+  "FAQ",
+];
+
+export const footerIcons = ["ig", "fb", "twitter", "mail", "twitch"];
