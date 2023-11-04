9a696f6e34c5be2d3f1ff7d085ba00e10af5644d
prasetion <prasetion@gmail.com>
2023-11-04 19:10:24 +0700

    add responsive banner for mobile

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -4,14 +4,14 @@ import "./style.css";
 const Banner = () => {
   return (
     <div className="banner-container">
-      <div className="banner-title">Sewa Mobil di (Lokasimu) Sekarang</div>
-      <div className="banner-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.{" "}
+      <div className="banner-info">
+        <div className="banner-title">Sewa Mobil di (Lokasimu) Sekarang</div>
+        <div className="banner-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+        </div>
       </div>
-      <div className="banner-button">
-        <Button name={"Mulai Sewa Mobil"}></Button>
-      </div>
+      <Button name={"Mulai Sewa Mobil"}></Button>
     </div>
   );
 };
