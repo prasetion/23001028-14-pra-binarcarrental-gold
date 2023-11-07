150173561867256ba400217d9b219079851c5fa0
prasetion <prasetion@gmail.com>
2023-11-07 19:12:38 +0700

    seperate data and viewer for faq

---
+++ src/datas/FAQData.js
@@ -0,0 +1,27 @@
+export const faqData = [
+  {
+    id: 0,
+    title: "Apa saja syarat yang dibutuhkan?",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 1,
+    title: "Berapa hari minimal sewa mobil lepas kunci?",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    title: "Berapa hari sebelumnya sabaiknya booking sewa mobil?",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    title: "Apakah Ada biaya antar-jemput?",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 4,
+    title: "Bagaimana jika terjadi kecelakaan?",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
