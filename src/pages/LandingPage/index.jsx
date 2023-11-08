b728ec7053930331075c3b791e750857d025240c
prasetion <prasetion@gmail.com>
2023-11-08 21:28:21 +0700

    add ourservices section

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -3,10 +3,12 @@ import Footer from "../../components/Footer";
 import WhyUs from "../../components/WhyUs";
 import Banner from "../../components/Banner";
 import FAQ from "../../components/FAQ";
+import OurServices from "../../components/OurServices";
 
 const LandingPage = () => {
   return (
     <div>
+      <OurServices />
       <WhyUs />
       <Banner />
       <FAQ />
