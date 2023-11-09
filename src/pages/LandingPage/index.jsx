42978b23d148105868889ff2280780980f1bfbfe
prasetion <prasetion@gmail.com>
2023-11-09 21:46:02 +0700

    add navbar, install react router dom

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -4,10 +4,12 @@ import WhyUs from "../../components/WhyUs";
 import Banner from "../../components/Banner";
 import FAQ from "../../components/FAQ";
 import OurServices from "../../components/OurServices";
+import Header from "../../components/Header";
 
 const LandingPage = () => {
   return (
     <div>
+      <Header />
       <OurServices />
       <WhyUs />
       <Banner />
