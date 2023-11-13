850924ae1b991b842a3137000890b5f01efcc05d
prasetion <prasetion@gmail.com>
2023-11-13 17:10:14 +0700

    modify Link to in logo for back to home page

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,10 +1,13 @@
 import logo from "../../assets/images/logo.png";
-// import "./style.css";
+import "./style.css";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <div>
-      <img src={logo} alt="logo" />
+    <div className="logo">
+      <Link to={"/"}>
+        <img src={logo} alt="logo" />
+      </Link>
     </div>
   );
 };
